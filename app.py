370712a9cc23caf69d4f9dfd636a29fe335aef41
Tuhin Das <tdas94965@gmail.com>
2026-04-26 16:18:35 +0530

    add image upload feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from google import genai
 import os
+import PIL.Image
 
 
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -20,6 +21,7 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
+uploaded_file = st.file_uploader("Upload image or file", type=["png", "jpg", "jpeg", "pdf", "txt"])
 
 user_input = st.chat_input("Ask me anything...")
 
@@ -28,12 +30,17 @@ if user_input:
     st.chat_message("user").write(user_input)
     st.session_state.messages.append({"role": "user", "content": user_input})
 
-    response = client.models.generate_content(
+    if uploaded_file is not None:
+        image = PIL.Image.open(uploaded_file)
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=[user_input, image]
+        )
+    else:
+         response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=user_input
     )
     reply = response.text
-
-    # Show assistant reply
     st.chat_message("assistant").write(reply)
     st.session_state.messages.append({"role": "assistant", "content": reply})
